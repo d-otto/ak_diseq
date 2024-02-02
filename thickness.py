e9d4644c626427e71c0deffa4249b56798e1995d
Daniel Otto <drotto@uw.edu>
2024-02-01 16:53:02 -0800

    Added table a1

--- thickness.py
+++ thickness.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 import pickle
 
+# todo: add script to get thickness from flowline geom
 def load_flowline_geom():
     fp = r"/Users/drotto/src/USGS/glacier-diseq/features/flowline_geom.pickle"
     with open(fp, 'rb') as f:
